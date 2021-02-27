28e89f1891d77921fe5bda487b429cb1a5304799
akh4rf <akh4rf@virginia.edu>
2021-02-27 16:30:37 -0500

    Changed base URL for site

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('hoosactive/', include('hoosactive.urls')),
+    path('', include('hoosactive.urls')),
     path('admin/', admin.site.urls),
 ]
