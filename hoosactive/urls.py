85653eb99971551b5f5ba89b236872ca73b0f59e
Jason Lee <ml5vt@virginia.edu>
2021-03-25 22:36:21 -0400

    lol

--- hoosactive/urls.py
+++ hoosactive/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
     path('register/', views.register, name="register"),
+    path('leaderboard/', views.leaderboard, name="leaderboard"),
+
 ]
