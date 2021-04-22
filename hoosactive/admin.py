3faf00a2919b04996a0c61b2936bfb6f78860843
Drew Tigges <apt6qp@virginia.edu>
2021-04-22 14:07:51 -0400

    Updates admin page

--- hoosactive/admin.py
+++ hoosactive/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
 from .models import Profile, Exercise, Entry, Workout
+from django.db.models import Count
 
 # Register your models here.
 
-admin.site.register(Profile)
+class entryAdmin( admin.ModelAdmin ):
+    list_display = ( 'user', 'city', 'exercise', 'date', 'calories', 'duration_hours' )
+
+class workoutAdmin( admin.ModelAdmin ):
+    list_display = ( 'user', 'desc', 'date' )
+
+class profileAdmin( admin.ModelAdmin ):
+    list_display = ( 'user', 'number_of_friends', 'number_of_friend_requests', 'show_stats' )
+
+    def number_of_friends( self, inst ):
+        return inst._number_of_friends
+
+    def number_of_friend_requests( self, inst ):
+        return inst._number_of_friend_requests
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset( request )
+        queryset = queryset.annotate(
+            _number_of_friends=Count( 'friends', distinct=True ),
+            _number_of_friend_requests=Count( 'friend_requests', distinct=True )
+        )
+        return queryset
+        
+
+admin.site.register(Profile, profileAdmin)
 admin.site.register(Exercise)
-admin.site.register(Entry)
-admin.site.register(Workout)
+admin.site.register(Entry, entryAdmin)
+admin.site.register(Workout, workoutAdmin)
