aef58d0e5b852839c5ba679102617ab620c9ec22
Austin Houck <akh4rf@virginia.edu>
2021-05-06 16:41:55 -0400

    Bug fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,7 +1,1 @@
-
-import socket
-host = socket.gethostbyname(socket.gethostname())
-if (host[0:3]=="172" or host[0:3]=="192" or host[0:3]=="127"):
-    from .local_settings import *
-else:
-    from .production_settings import *
+from .production_settings import *
