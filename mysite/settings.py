ceaa13226e1f15149516dbe233ee3cba679986b8
Ian Sohan <iangsohan@gmail.com>
2021-05-06 14:34:37 -0400

    README update

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,6 @@
 
 import socket
-if socket.gethostbyname(socket.gethostname())[0:3]=="127":
+if socket.gethostbyname(socket.gethostname())[0:3]=="172":
     from .local_settings import *
 else:
     from .production_settings import *
